662d27e2474f190ef0a8fdfb45286fa66ba398a2
aoalfred <aalfred@gmail.com>
2017-05-25 18:54:42 -0700

    started class for parsing per site

--- FakeNewsProfiler/Default.aspx.cs
+++ FakeNewsProfiler/Default.aspx.cs
@@ -14,32 +14,11 @@ namespace FakeNewsProfiler
         public string htmlOutput;
         protected void Page_Load(object sender, EventArgs e)
         {
-            string[] input = { "http://www.cnn.com", "http://www.foxnews.com", "http://www.breitbart.com", "http://www.msnbc.com", "http://www.drudgereport.com" };
-            htmlOutput = "<table>";
-            List<string> Urls = new List<string>(input);
-            foreach (var Url in Urls)
-            {
-                string htmlStripText = GetWebText(Url);
-                htmlOutput += "<tr><td>";
-                htmlOutput += (htmlStripText);
-                htmlOutput += "</td></tr>";
-            }
+            string htmlStripText = SiteParsers.FoxNewsParser("http://www.foxnews.com");
+            htmlOutput += "<tr><td>";
+            htmlOutput += (htmlStripText);
+            htmlOutput += "</td></tr>";
             htmlOutput += "</table>";
-        }
-        private static string GetWebText(string args)
-        {
-            var web = new HtmlWeb();
-            var doc = web.Load(args);
-            HtmlNode htmlText = doc.DocumentNode.SelectSingleNode("//a");
-            if (htmlText != null)
-            {
-                return htmlText.InnerHtml;
-            }
-            else
-            {
-                return ("Broken Pull");
-            }
-
         }
     }
 }
